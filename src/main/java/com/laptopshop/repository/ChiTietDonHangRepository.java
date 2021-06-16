c68128760718ec42a8ae25dce2fdcd10be2575b1
quocdat-CtrlS <67988595+quocdat-CtrlS@users.noreply.github.com>
2021-06-16 21:18:53 +0700

    first commit

---
+++ src/main/java/com/laptopshop/repository/ChiTietDonHangRepository.java
@@ -0,0 +1,8 @@
+package com.laptopshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.laptopshop.entities.ChiTietDonHang;
+
+public interface ChiTietDonHangRepository extends JpaRepository<ChiTietDonHang, Long>{
+}
